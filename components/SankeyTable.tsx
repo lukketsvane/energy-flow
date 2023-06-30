963d7bf322424456085ead739fcc1849149f34cd
unknown <iverfinne@gmail.com>
2023-06-30 16:00:39 +0200

    clean up sankey code and split into multiple components to reduce filelength

--- components/SankeyTable.tsx
+++ components/SankeyTable.tsx
@@ -1,26 +1,28 @@
+// SankeyTable.tsx
+
 import { TableProps } from './types';
 
 export function SankeyTable({ data, handleEdit, handleRemove }: TableProps) {
   return (
-    <table className="w-full mt-8">
+    <table className="w-full table-auto border-collapse">
       <thead>
         <tr>
           <th>From</th>
           <th>To</th>
           <th>Weight</th>
-          <th>Action</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         {data.map((row, index) => (
           <tr key={index}>
-            {row.map((cell, i) => (
-              <td key={i}>
-                <input type="text" name={i.toString()} value={cell} onChange={(e) => handleEdit(index, e)} className="rounded border border-gray-400 p-2 w-full" />
+            {row.map((cell, cellIndex) => (
+              <td key={cellIndex}>
+                <input type="text" name={cellIndex.toString()} value={cell} onChange={(e) => handleEdit(index, e)} className="rounded border border-gray-400 p-2" />
               </td>
             ))}
             <td>
-              <button onClick={() => handleRemove(index)} className="py-2 px-4 rounded bg-red-500 text-white">Remove</button>
+              <button onClick={() => handleRemove(index)} className="rounded bg-red-500 text-white px-4 py-2">Remove</button>
             </td>
           </tr>
         ))}
