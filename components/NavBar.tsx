a9b73c477e2978a81842c50065f7b84eca82985f
unknown <iverfinne@gmail.com>
2023-06-30 14:09:54 +0200

    add mobile TopNav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,28 +1,48 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import Link from 'next/link';
-import HelpDropdown from './HelpDropdown';  // import your HelpDropdown component
+import HelpDropdown from './HelpDropdown';
 
 export default function TopNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  const toggleModal = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="flex flex-row w-full items-center space-y-2.5">
-      <div className="flex justify-between items-center px-6 py-4 bg-white shadow-md w-full">
+    <nav className="flex flex-row w-full items-center space-y-2.5 px-6 py-4 bg-white shadow-md">
+      <div className="flex justify-between items-center w-full">
         <div className="flex items-center space-x-4">
-          <Image src="/logo.png" width={52} height={21} alt="logo" />
+          {router.pathname === '/' ? (
+            <Image src="/logo.png" width={52} height={21} alt="logo" />
+          ) : (
+            <Link href="/">
+              <Image src="/logo.png" width={52} height={21} alt="logo" />
+            </Link>
+          )}
           <h1 className="text-lg font-bold">FlowViz 🧙</h1>
-          <Link href="/">
-            <Image src="/back-button.svg" width={16.00} height={26.20} alt="back button" />
-          </Link>
-          <Link href="/docs">
-            <span className="text-base font-medium text-grey-600 hover:text-blue-800">Docs</span>
-          </Link>
-          <Link href="/help">
-            <span className="text-base font-medium text-grey-600 hover:text-blue-800">Help centre</span>
-          </Link>
-          <Link href="/faq">
-            <span className="text-base font-medium text-grey-600 hover:text-blue-800">FAQ</span>
-          </Link>
+          <div className="hidden md:flex items-center space-x-4 ml-4">
+            <Link href="/">
+              <Image src="/back-button.svg" width={20} height={20} alt="back button" />
+            </Link>
+            <Link href="/docs">
+              <span className="text-base font-medium text-grey-600 hover:text-blue-800">Docs</span>
+            </Link>
+            <Link href="/help">
+              <span className="text-base font-medium text-grey-600 hover:text-blue-800">Help centre</span>
+            </Link>
+            <Link href="/faq">
+              <span className="text-base font-medium text-grey-600 hover:text-blue-800">FAQ</span>
+            </Link>
+          </div>
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 md:hidden">
+          <button onClick={toggleModal}>
+            <Image src="/hamburger_icon.png" width={24} height={24} alt="menu" />
+          </button>
+        </div>
+        <div className="hidden md:flex items-center space-x-4">
           <Link href="#">
             <Image src="/code-button.svg" width={18.12} height={15.34} alt="code button" />
           </Link>
@@ -32,8 +52,28 @@ export default function TopNav() {
           <Link href="#">
             <Image src="/settings-button.svg" width={20.13} height={20.13} alt="settings button" />
           </Link>
-          <HelpDropdown />   {/* add your HelpDropdown component here */}
+          <div className="hidden md:block">
+            <HelpDropdown />
+          </div>
         </div>
+        {isOpen && (
+          <div className="fixed inset-0 flex items-start justify-center z-50 pt-20">
+            <div className="bg-white p-5 w-full h-full overflow-auto">
+              <button onClick={toggleModal}>Close</button>
+              <div className="space-y-2 mt-5">
+                <Link href="/docs">
+                  <span className="text-base font-medium text-grey-600 hover:text-blue-800 block">Docs</span>
+                </Link>
+                <Link href="/help">
+                  <span className="text-base font-medium text-grey-600 hover:text-blue-800 block">Help centre</span>
+                </Link>
+                <Link href="/faq">
+                  <span className="text-base font-medium text-grey-600 hover:text-blue-800 block">FAQ</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
